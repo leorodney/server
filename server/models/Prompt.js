f8241041e7f9c60e41b0f211192d2c2939908c52
whybe7 <whybe7@hotmail.com>
2023-03-20 22:44:10 +0100

    The base structure build #2

---
+++ server/models/Prompt.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Prompt = new mongoose.Schema({
+    author: { type: String, required: true },
+    value: { type: String, required: true },
+    img: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    timeStamp: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", User);
