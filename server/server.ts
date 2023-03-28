fdb5d904418f8f920f3b7637c2a25fa144f37252
whybe7 <whybe7@hotmail.com>
2023-03-28 19:53:06 +0200

    fixing missing HTTP request limiter

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import cors from "cors";
 // Mongodb connection
 import { mongoConnect } from "./database/connect";
@@ -8,12 +9,18 @@ import { communityRoute } from "./routes/community";
 import { productionRoute } from "./routes/production";
 import { promptRoute } from "./routes/prompt";
 
-
-//routes
-
+// init the server:
 const server = express();
+
+// setup rate limiter
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 10 // limit each IP to 10 requests per 1 minutes
+});
 //Middelwares:
 server.use(cors());
+//  apply to all requests
+server.use(limiter);
 server.use(express.json({limit: '50mb'}));
 dotenv.config();
 
