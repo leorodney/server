60a78fcc8b18f4c3b74e24b92760c509bd06130d
whybe7 <whybe7@hotmail.com>
2023-04-01 01:58:49 +0200

    mapping auth routes for both login or signin...

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Community from "./pages/Community";
+import Login from "./pages/Login";
 import Production from "./pages/Production";
+import Register from "./pages/Register";
 
 export default function Router() {
   return (
@@ -8,6 +10,12 @@ export default function Router() {
         <Routes>
             <Route index path="/" element={<Community />} />
             <Route path="/production" element={<Production />} />
+            {/* Point the same Login page for both "/login" and "/signin" routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signin" element={<Login />} />
+            {/* Point the same Register page for both "/login" and "/signin" routes */}
+            <Route path="/register" element={<Register />} />
+            <Route path="/signup" element={<Register />} />
         </Routes>
     </BrowserRouter>
   )
