c2fa7b5ed3ec281d2b62635ef9cfac585e558f68
whybe7 <whybe7@hotmail.com>
2023-04-01 02:09:42 +0200

    referenc the User in his created Prompt

--- server/src/@types/Prompt.d.ts
+++ server/src/@types/Prompt.d.ts
@@ -1,8 +1,8 @@
 // declare a module named 'prompt' with a default export of the above interface
-declare module 'prompt' {
-    const prompt: {
+declare module 'Prompt' {
+    const Prompt: {
         (path: string): Promise<boolean>;
         sync(path: string): boolean;
     };
-    export = prompt;
+    export = Prompt;
 };
