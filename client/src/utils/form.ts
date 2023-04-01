3789ff22c7532683720bd06c5cc8468ed15a7fb6
whybe7 <whybe7@hotmail.com>
2023-04-01 02:02:02 +0200

    form fields utils

---
+++ client/src/utils/form.ts
@@ -0,0 +1,47 @@
+export const loginFields = [
+    {
+        name: 'emailorusername',
+        label: 'Email or Username',
+        type: 'text',
+        placeholder: 'Email or Username',
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        type: 'password',
+        placeholder: 'Password',
+    }
+];
+
+export const registerFields = [
+    {
+        name: 'fullname',
+        label: 'Full Name',
+        type: 'text',
+        placeholder: 'Full Name',
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        type: 'text',
+        placeholder: 'Email',
+    },
+    {
+        name: 'username',
+        label: 'Username',
+        type: 'text',
+        placeholder: 'Username',
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        type: 'password',
+        placeholder: 'Password',
+    },
+    {
+        name: 'confirmPassword',
+        label: 'Confirm Password',
+        type: 'password',
+        placeholder: 'Confirm Password',
+    }
+];
