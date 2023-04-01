12d4f77640a22d8f4893c6d402126a19a681bc2f
whybe7 <whybe7@hotmail.com>
2023-04-01 02:10:51 +0200

    auth routes handlers (auth api-endpoints)

---
+++ server/routes/login.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+import bcrypt from 'bcrypt';
+
+export const loginRoute = async (req: Request, res: Response)=>{
+    const {usernameOrEmail, password} = req.body;
+    try{
+        const user = await User.findOne({$or: [{username: usernameOrEmail}, {email: usernameOrEmail}]});
+        if(!user){ 
+            return res.status(404).json({message: 'User not found'}); 
+        }
+        if(!await bcrypt.compare(password, user.password)){
+            return res.status(401).json({message: 'Invalid password or Email/Username'});
+        }
+        res.status(200).json({message: 'Login successful'});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send();
+    }
+};
