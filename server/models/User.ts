54462001c2ea7f6b5e9fe711fe2a04e77b494af9
whybe7 <whybe7@hotmail.com>
2023-04-01 02:10:12 +0200

    init the User DB Schema

---
+++ server/models/User.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('User', User);
