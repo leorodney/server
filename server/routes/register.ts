ad831d668713fa65a7c4980ca1f56aa3c71ed2c2
whybe7 <whybe7@hotmail.com>
2023-04-04 01:35:47 +0200

    automatically create session when user register

--- server/routes/register.ts
+++ server/routes/register.ts
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 
 export const registerRoute = async (req: Request, res: Response)=>{
     const {firstname, lastname, email, username, password} = req.body;
+
     try{
         if(await User.findOne({email: { $eq: email }})){
             return res.status(409).json({message: `User with email: ${email}, already exists`});
@@ -14,6 +15,16 @@ export const registerRoute = async (req: Request, res: Response)=>{
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({firstname, lastname, email, username, password: hashedPassword});
         user.save();
+        req.session.user = {
+            authenticated: true,
+            username: user.username,
+            uid: user._id
+        };
+        req.session.save((err)=>{
+            if(err){
+                console.error(err);
+            }
+        });
         res.status(201).json({message: 'User created successfully'});
     }
     catch(error){
