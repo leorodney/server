5794cae47d483626d389a1be257df47f0fa8ef87
whybe7 <whybe7@hotmail.com>
2023-04-09 02:47:51 +0200

    adding types desc  for the mongoConnect func

--- server/database/connect.ts
+++ server/database/connect.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
+/**
+ * establish connection to mongoDB
+ * @param uri 
+ * @param dbName 
+ */
 export function mongoConnect(uri:string, dbName:string): void{
     mongoose.set("strictQuery", true);
     mongoose.connect(uri, {
