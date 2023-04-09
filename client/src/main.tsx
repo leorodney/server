8b19ab3be1715f7600ca4edae4c1766f7ba7ade6
whybe7 <whybe7@hotmail.com>
2023-04-09 02:27:18 +0200

    emplemet store provider in the entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './router';
 import './styles/index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router/>
+    <Provider store={store}>
+      <Router/>
+    </Provider>
   </React.StrictMode>,
 )
