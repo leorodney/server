07c3e960bd43a0f79125b3e8cde2ccf3792b039f
whybe7 <whybe7@hotmail.com>
2023-04-09 02:31:38 +0200

    making the prompt card #45

--- client/src/components/Prompt.tsx
+++ client/src/components/Prompt.tsx
@@ -1,10 +1,19 @@
 import { Prompt } from '../interfaces/prompt'
 
-export default function Prompt(prompt: Prompt) {
-    console.log(prompt)
+export default function Prompt({ id, author, img, value, downloads, likes }: Prompt) {
   return (
-  <>
-    
-  </>
+    <article id={id} className="relative h-64 group flex justify-center items-center rounded-lg overflow-hidden border border-slate-800 bg-slate-800 bg-center bg-cover bg-no-repeat" style={{backgroundImage: `url(${img})`}}>
+      <button className='absolute left-2 top-[-50%] group-hover:top-2 px-4 py-1 text-white transition-all hover:scale-[1.02] active:scale-95 rounded-lg bg-[var(--clr-p)]'>Use It</button>
+      <div className='absolute w-full py-1 text-white max-h-[60%] bottom-[-120%] bg-slate-900 bg-opacity-[0.7] flex justify-between items-center gap-2 flex-col transition-all group-hover:bottom-0'>
+        <div className='w-full px-4 flex justify-between items-center'>
+          <h3 className='capitalize text-teal-600'>{author}</h3>       
+          <div className='flex justify-between items-center'>
+            <button>{downloads}</button>
+            <button>{likes}</button>
+          </div>
+        </div>
+        <p className='px-4 text-sm' title={value}>{value}</p>
+      </div>
+    </article>
   )
 }
