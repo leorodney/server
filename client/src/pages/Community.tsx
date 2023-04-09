bd06fe3d6502b545512acd216026a812d16ddff2
whybe7 <whybe7@hotmail.com>
2023-04-09 02:36:06 +0200

    debugging the stored auth in redux

--- client/src/pages/Community.tsx
+++ client/src/pages/Community.tsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
 import Hero from "../components/Hero";
 import ShowCases from "../components/ShowCases";
+import { AuthUser } from "../interfaces/user";
 
 export default function Community() {
+  const user = useSelector((state: AuthUser)=> state);
+
+  console.log(user);
+
   return (
     <>
       <Hero/>
