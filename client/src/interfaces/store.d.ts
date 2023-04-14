c00f9ecdc06d5763d5bb30184bfdbb53d093d5a5
whybe7 <whybe7@hotmail.com>
2023-04-14 20:02:14 +0200

    Refactor Store interface with additional reducers for Prompts and Production
    - Restructured and updated StoreReducers and StoreState to include additional reducers for Prompts and Production
    - Prompts now uses the new PromptState type from prompt interface
    - Production now uses the new ProductionState type from production interface
    - Updated import for PayloadAction from redux toolkit

--- client/src/interfaces/store.d.ts
+++ client/src/interfaces/store.d.ts
@@ -1,39 +1,49 @@
 import { AnyAction, Reducer } from "redux";
-import { Prompts } from "./prompt";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { PromptState, Prompts } from "./prompt";
 import { AuthUser } from "./user";
 import { Prompt } from "./prompt";
+import { ProductionState } from "./production";
+import { setPrompt, setStatus } from "../store/productionSlice";
+
 
 /** 
  * declare interface  for store redux reducers
  * @interface StoreReducers
  * @property {Reducer<AuthUser, AnyAction>} user - user reducer
- * @property {Reducer<Prompt[], AnyAction>} prompts - prompts reducer
+ * @property {Reducer<PromptState, AnyAction>} prompts - prompts reducer
+ * @property {Reducer<ProductionState, PayloadAction<ProductionState["status"]> | PayloadAction<ProductionState["prompt"]>>} production - production reducer
  * @example
  * const store = createStore(combineReducers<StoreReducers>({
  *    user: authReducer,
- *   prompts: promptsReducer
+ *    prompts: promptsReducer,
+ *    production: productionReducer
  * }));
  * 
  * */
 
 export interface StoreReducers {
     user: Reducer<AuthUser, AnyAction>;
-    prompts: Reducer<Prompt[], AnyAction>;
+    prompts: Reducer<PromptState, AnyAction>;
+    production: Reducer<ProductionState, PayloadAction<ProductionState["status"]> | PayloadAction<ProductionState["prompt"]>>;
 }
 
 /**
  * declare interface for store redux state
  * @interface StoreState
- * @property {AuthUser} user - user state
- * @property {Prompt[]} prompts - prompts state
+ * @property {AuthUser} `user` - user state
+ * @property {PromptState} `prompts` - prompts state
+ * @property {ProductionState} `production` - production state
  * @example
- * const store = createStore(combineReducers<StoreReducers>({
+ * const store = createStore(combineReducers({
  *   user: authReducer,
- *  prompts: promptsReducer
- * }));
+ *   prompts: promptsReducer,
+ *   production: productionReducer
+ * } satisfies StoreReducers)));
  */
 
 export interface StoreState {
     user: AuthUser;
-    prompts: Prompt[];
+    prompts: PromptState;
+    production: ProductionState;
 }
