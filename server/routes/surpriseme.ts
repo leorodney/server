a748c889b08f3c1a72e42729ef20c5f3c21456ff
whybe7 <whybe7@hotmail.com>
2023-04-14 20:21:23 +0200

    feat: Add surprise me route functionality
    This commit adds a new route that generates a "Surprise Me" prompt using the OpenAI DALL-E model and GPT-3. The server sends the generated prompt to the client and logs the result.

---
+++ server/routes/surpriseme.ts
@@ -0,0 +1,31 @@
+// surprise me route
+import { Request, Response } from 'express';
+import dotenv from 'dotenv';
+// openai dall-e model
+import { OpenAIApi } from 'openai';
+// openai config
+import { openaiConfig, surpriseMePromptConfig } from '../configs';
+// load .env variables
+dotenv.config();
+
+const GPT = new OpenAIApi(openaiConfig);
+
+/**
+ * Generate `Prompt` as `Suprise Me Prompt` using the `openai` api and the **`gpt-3`** model
+ * @param req 
+ * @param res 
+ */
+export const surprisemeRoute = async (req: Request, res: Response)=>{
+    try{
+        // sending generated prompts to the client using the openai api and the gpt-3 model
+        const GPTResponse = await GPT.createCompletion(surpriseMePromptConfig);
+        // remove the '\n' and '\t'... characters from the prompt
+        const surprisemePrompt = GPTResponse.data.choices[0].text?.replace(/[\\\r\t\n]/g, "");
+        res.status(200).json({prompt: surprisemePrompt});
+        console.log("\t[ SURPRISE ME PROMPT ]: ", surprisemePrompt);
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send();
+    }
+};
