8929b4774be3a8979e6658687ef0f7f1805ef2a2
whybe7 <whybe7@hotmail.com>
2023-04-14 19:49:49 +0200

    Update search functionality in Hero component
    The search input element in Hero component now updates the search query state on every text input and dispatches a search query on 'Enter' key.

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
     <main className='h-[80vh] w-screen text-white flex items-center justify-center gap-10 flex-col bg-cover bg-center bg-no-repeat backdrop:brightness-75' style={{backgroundImage: `url(${HeroBG})`}}>
       <Header/>
       <div className='h-14 w-[70%] flex items-center justify-between gap-4'>
-        <input className='h-full w-full px-4 py-2 text-black bg-white rounded-lg focus:outline-[var(--clr-p)]' type="text" placeholder='Search in the community...'/>
+        <input onChange={e => setSearchQuery(e.target.value)} onKeyDown={searchQueryDispatcher} className='h-full w-full px-4 py-2 text-black bg-white rounded-lg focus:outline-[var(--clr-p)]' type="text" placeholder='Search in the community...'/>
         <button className='h-full px-10 py-2 text-white text-lg transition-all hover:scale-[1.02] active:scale-95 rounded-lg bg-[var(--clr-p)]'><Link to={"/production"}>Generate</Link></button>
       </div>
     </main>
