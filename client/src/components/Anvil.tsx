dc456581673636be0a76ab33070a5b2840e6d482
whybe7 <whybe7@hotmail.com>
2023-04-14 20:12:12 +0200

    refactor: Use the value prop for textarea and rename "Surpris Me" button
    The textarea onChange event now uses "value" prop instead of capturing value directly to state. The "Surpris Me" button was renamed to "Surprise Me".

--- client/src/components/Anvil.tsx
+++ client/src/components/Anvil.tsx
@@ -63,8 +63,8 @@ export default function Anvil() {
   return (
     <form onSubmit={generatePrompt} className="h-full w-1/2 px-2 flex items-center justify-center gap-4 flex-col ">
         <div className="h-full w-full relative flex items-center">
-            <textarea onChange={capturePrompt} className="w-full h-full px-6 py-4 text-black bg-white drop-shadow-md rounded-lg focus:outline-[var(--clr-p)]" cols={30} rows={10} placeholder="Start prompting here..."></textarea>
-            <button className="absolute right-2 bottom-2 px-4 py-2 text-white transition-all hover:scale-[1.02] active:scale-95 rounded-lg bg-[var(--clr-p)]" type="button">Surpris Me</button>
+            <textarea onChange={capturePrompt} value={prompt.value} className="w-full h-full px-6 py-4 text-black bg-white drop-shadow-md rounded-lg focus:outline-[var(--clr-p)]" cols={30} rows={10} placeholder="Start prompting here..."></textarea>
+            <button onClick={surprisemePrompt} className="absolute right-4 bottom-4 px-4 py-2 text-white transition-all hover:scale-[1.02] active:scale-95 rounded-lg bg-[var(--clr-p)]" type="button">Surpris Me</button>
         </div>
         <div className="w-full flex justify-between items-center gap-4">
             <button className="h-full w-full px-4 py-2 text-white transition-all hover:scale-[1.02] active:scale-95 rounded-lg bg-[var(--clr-p)]" type="submit">Generate</button>
