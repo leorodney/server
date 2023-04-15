0958198d326a3d0e3ef58cc87e45843f6afb37f4
whybe7 <whybe7@hotmail.com>
2023-04-15 20:30:35 +0200

    Refactor display logic in Motivation component.
    Changed the display size and layout of the motivational image to be more appealing. Default "Leorodney" for missing author information.

--- client/src/components/Motivation.tsx
+++ client/src/components/Motivation.tsx
@@ -5,9 +5,9 @@ export default function Motivation({author, value, img} : Prompt) {
   return (
     <div className="h-[100%] w-[100%] text-white flex items-center justify-around flex-col bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url(${img})`}}>
         <h1 className="text-5xl font-serif">LEORDONEY</h1>
-        <div className="h-72 w-72 bg-cover bg-center bg-no-repeat overflow-hidden rounded-2xl" style={{backgroundImage: `url(${img ? img : artPlaceholder})`}}></div>
+        <div className="h-80 w-80 bg-cover bg-center bg-no-repeat overflow-hidden rounded-2xl" style={{backgroundImage: `url(${img ? img : artPlaceholder})`}}></div>
         <p>{value}</p>
-        <h3>Prompt Art By - {author}</h3>
+        <h3>Prompt Art By - {author || "Leorodney"}</h3>
     </div>
   )
 }
