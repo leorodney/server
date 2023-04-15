a54c3555ee60ab9d14b8f5d4cef551373efc4d8e
whybe7 <whybe7@hotmail.com>
2023-04-15 20:32:24 +0200

    Refactor ShowCases component code for searchQuery and section className values - Removes 'relative' from section className to minimize duplication - Adds span element with search query if searchQuery parameter exists, to improve user experience.

--- client/src/components/ShowCases.tsx
+++ client/src/components/ShowCases.tsx
@@ -14,7 +14,7 @@ export default function ShowCases() {
       searchQuery ? <h2 className="ml-6 my-6 text-2xl font-medium dark:text-white">The Community Showcases For: "<span className="text-[var(--clr-p)] underline">{searchQuery}</span>"</h2> 
       : <h2 className="ml-6 my-6 text-2xl font-medium dark:text-white">The Community Showcases:</h2>
     }
-    <section className="relative px-4 grid lg:grid-cols-4 gap-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
+    <section className="px-4 grid lg:grid-cols-4 gap-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
         <Prompt
             img={HeroBG} 
             author="whybe" 
