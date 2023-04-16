4333aafd3ecc8a6c2b5e7ce22a36d1a16c565ceb
whybe7 <whybe7@hotmail.com>
2023-04-16 16:07:32 +0200

    Refactor authentication in Community page using a custom React hook
    Instead of manually checking authentication and navigating to login, useAuthorization hook is now utilized for authentication check and redirection, leading to simpler and cleaner code.

--- client/src/pages/Community.tsx
+++ client/src/pages/Community.tsx
@@ -1,13 +1,10 @@
 import Hero from "../components/Hero";
 import ShowCases from "../components/ShowCases";
-import { useNavigate } from "react-router-dom";
 import useAuthorization from "../hooks/authorization";
 import Production from "../components/Production";
 
 export default function Community() {
-  const authorization = useAuthorization();
-  const navigate = useNavigate();
-  !authorization.isAuthenticated ? navigate("/login") : null;
+  useAuthorization("/login");
   return (
     <>
       <Hero/>
