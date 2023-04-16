006d103a6756e387db1df6070381e07d7b42a00a
whybe7 <whybe7@hotmail.com>
2023-04-16 15:34:32 +0200

    Add new styles to the app and update the import in main.tsx
    This commit adds a new style file 'app.css' and updates the import statement in main.tsx to include this file. The new styles will be applied to the app.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './router';
-import './styles/index.css';
 import { Provider } from 'react-redux';
 import store from './store';
+// Styles
+import './styles/index.css';
+import './styles/app.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
