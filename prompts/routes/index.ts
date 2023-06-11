493543115df9978b0c761a96ad3f336b72323d4b
vvhybe <vvhybe@hotmail.com>
2023-06-11 13:26:34 +0100

    retrive user prompts

--- prompts/routes/index.ts
+++ prompts/routes/index.ts
@@ -8,7 +8,25 @@ export const communityRoute = async (req: Request, res: Response)=>{
         res.status(200).json(communityShowcases);
     }
     catch(error){
-        console.log(error);
+        console.error(error);
         res.status(500).send();
     }
+}
+
+export const userPromptsRoute = async (req: Request, res: Response)=>{
+    try{
+        // finding the user prompts using the prompt author or the user uid,
+        // it should be in this order: author, uid; to avoid the error:
+        //CastError: Cast to ObjectId failed for value "whybe" (type string) at path "uid" for model "Prompt"
+        console.log(req.session?.user);
+        const userPrompts = await Prompt.find({ author: { $eq: req.params.uid } });
+        // calculate the total downloads/likes user prompts reached
+        let totalDownloads = userPrompts.reduce((accumulator, prompt) => accumulator + prompt.downloads, 0);
+        let totalLikes = userPrompts.reduce((accumulator, prompt) => accumulator + prompt.likes, 0);
+        res.status(200).json({prompts: userPrompts, totalDownloads, totalLikes});
+
+    } catch(error){
+        console.error(error);
+        res.status(500).send();
+    };
 }
