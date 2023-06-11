aa1482e295e58895f3fc6f59ac41f601a02b080a
vvhybe <vvhybe@hotmail.com>
2023-06-11 13:37:21 +0100

    reconstruct user schema and adding user profile img

--- auth/model/User.ts
+++ auth/model/User.ts
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+import { Schema, model } from 'mongoose';
 
 const User = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     username: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    profilePicture: { type: String, default: process.env.CLOUDINARY_DEFAULT_PROFILE_IMAGE },
 }, {
     timestamps: true
 });
